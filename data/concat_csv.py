d3f1e39d49f3e5e5484a7c65cfc42050cd06d705
rhzphlv <rheza.jogja@gmail.com>
2021-05-17 08:30:26 +0700

    sudah disatukan

---
+++ data/concat_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+df1 = pd.read_csv('lamudi/csv_(agak)bersih.csv')
+df2 = pd.read_csv('olx/csv_(agak)bersih.csv')
+df3 = pd.read_csv('rumah123/Stage_1_Data_Munging/munged_data_rumah123.csv')
+df3 = df3[['kamar_mandi','kamar','lb','lt','tipe_property','price','sertifikat','deskripsi',
+     'lokasi','ada_garasi','pool','taman','electricity','floors_total','gym']]
+
+data = pd.concat([df1,df2,df3],axis=0)
+data.to_csv('data.csv')
+
